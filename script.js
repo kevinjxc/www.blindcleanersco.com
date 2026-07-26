2b6c9e898673ddfc839518bf901de4c13104c1d5
Kevin Chen <3kechen@gmail.com>
2026-07-25 18:56:09 -0700

    Website revisions for animations

--- script.js
+++ script.js
@@ -1,25 +1,20 @@
-//Hamburger Variables Created
 const hamburger = document.querySelector(".hamburger");
 const menu = document.querySelector(".menu");
 
-//Event Listener for Hamburger Created
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     menu.classList.toggle("active");
 })
 
-//Lists Items in Hamburger
 document.querySelectorAll(".item").forEach(n => n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     menu.classList.remove("active");
 }))
 
-//Checks for Safari
 function isSafari() {
   return /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 }
 
-//Adds Indent to Safari
 if (isSafari()) {
   var indent1 = document.getElementById("indent1");
   indent1.innerHTML = "<br>";
@@ -31,42 +26,34 @@ if (isSafari()) {
   indent3.innerHTML = "<br>";
 }
 
-//Redirects to About Page
 function pageAbout() {
     window.location.href = "./about";
 }
 
-//Redirects to Services Page
 function pageServices() {
     window.location.href = "./services";
 }
 
-//Redirects to Jobs Page
 function pageJobs() {
     window.location.href = "./jobs";
 }
 
-//Redirects to Contact Page
 function pageContact() {
     window.location.href = "./contact";
 }
 
-//Redirects to Index Page
 function pageIndex() {
     window.location.href = "https://www.blindcleanersco.com/";
 }
 
-//Redirects to Email
 function pageEmail() {
     window.location.href = "mailto:blindcleanersco.com";
 }
 
-//Redirects to Phone Number
-function pagePhone() {
-    window.location.href = "tel:+12066591265";
-}
-
-//Redirects to Seattle Map
 function pageLocation() {
     window.open("https://www.google.com/maps/place/Seattle,+WA");
 }
+
+document.querySelectorAll('.current-year').forEach((yearElement) => {
+    yearElement.textContent = new Date().getFullYear();
+});
